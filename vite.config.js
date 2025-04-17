110483b45014c27da9ebde62f1da826c254637b4
JuiHsuanLee0303 <juihsuanlee0303@gmail.com>
2025-04-17 18:14:18 +0800

    fix: fix deploy problem

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: "/e-commerce-sample/",
-  build: {
-    assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
 });
