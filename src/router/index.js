974b5c6b9fa80033d35c9e4987bfe9a623fed239
JuiHsuanLee0303 <juihsuanlee0303@gmail.com>
2025-04-17 17:32:52 +0800

    fix: fix url problems with router

--- src/router/index.js
+++ src/router/index.js
@@ -34,7 +34,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory("/e-commerce-sample/"),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
